09d9140550f5f847cd7e807b8d4386143592950e
faizantariq1 <33232649+faizantariq1@users.noreply.github.com>
2025-02-15 18:31:49 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,7 @@ days = st.number_input("Enter Days to Search (1-30):", min_value=1, max_value=30
 
 # List of broader keywords
 keywords = [
- "Affair Relationship Stories", "Reddit Update", "Reddit Relationship Advice", "Reddit Relationship", 
-"Reddit Cheating", "AITA Update", "Open Marriage", "Open Relationship", "X BF Caught", 
-"Stories Cheat", "X GF Reddit", "AskReddit Surviving Infidelity", "GurlCan Reddit", 
-"Cheating Story Actually Happened", "Cheating Story Real", "True Cheating Story", 
-"Reddit Cheating Story", "R/Surviving Infidelity", "Surviving Infidelity", 
-"Reddit Marriage", "Wife Cheated I Can't Forgive", "Reddit AP", "Exposed Wife", 
-"Cheat Exposed"
+ "valentine's day horror stories", "night drive horror stories", "facebook horror story", "airbnb scary stories", "horror story", "forgotten VHS tape", "true horror", "creepy", "paranormal activity", "scary stories", "haunted", "horror short", "chilling story", "horror community", "creepy tales", "urban legends", "supernatural horror", "horror narration", "scary short film", "horror animation", "true scary stories", "horror fans", "horror content", "horror YouTube"
 ]
 
 # Fetch Data Button
